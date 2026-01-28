47e3427ffc5d83d5d3b263c4393c85d6b2fa29a6
Dominic Camill <camilldominic@gmail.com>
2026-01-27 16:51:40 -0800

    attempt to implement rpi hardware level functionality

--- state/hardware/hardware.go
+++ state/hardware/hardware.go
@@ -3,108 +3,72 @@
 package hardware
 
 import (
+	"fmt"
+	"log"
+	"time"
+
 	"github.com/LikeMyGames/FRC-Like-Robot/state/constantTypes"
-	"github.com/LikeMyGames/FRC-Like-Robot/state/pid"
 	"github.com/warthog618/go-gpiocdev"
+	"periph.io/x/conn/v3/driver/driverreg"
+	"periph.io/x/conn/v3/physic"
+	"periph.io/x/conn/v3/spi"
+	"periph.io/x/conn/v3/spi/spireg"
 )
 
-type (
-	SwerveModule struct {
-		DriveAngle        float64
-		AzimuthAngle      float64
-		DriveMotorCanID   int
-		AzimuthMotorCanID int
-	}
-
-	Device struct {
-		id     int64
-		value  float64
-		target float64
-	}
-
-	MotorController struct {
-		device        Device
-		PidController *pid.PIDController
-	}
-)
+type ()
 
 var (
 	config constantTypes.Battery = constantTypes.Battery{}
 )
 
+func OpenSpi() {
+	// Make sure periph is initialized.
+	// TODO: Use host.Init(). It is not used in this example to prevent circular
+	// go package import.
+	if _, err := driverreg.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Use spireg SPI port registry to find the first available SPI bus.
+	p, err := spireg.Open("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer p.Close()
+
+	// Convert the spi.Port into a spi.Conn so it can be used for communication.
+	c, err := p.Connect(physic.MegaHertz, spi.Mode3, 8)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Write 0x10 to the device, and read a byte right after.
+	write := []byte{0x10, 0x00}
+	read := make([]byte, len(write))
+	if err := c.Tx(write, read); err != nil {
+		log.Fatal(err)
+	}
+	// Use read.
+	fmt.Printf("%v\n", read[1:])
+}
+
+func Pin() {
+	v := 0
+	l, err := gpiocdev.RequestLine("gpiochip0", 4, gpiocdev.AsOutput(v))
+	if err != nil {
+		panic(err)
+	}
+	for {
+		<-time.After(time.Second)
+		v ^= 1
+		l.SetValue(v)
+	}
+}
+
 func CheckStatus() bool {
 	err := gpiocdev.IsChip("gpiochip0")
 	return err != nil
 }
-
-func (m *SwerveModule) ReadAzimuthAngle() float64 {
-
-	return 0
-}
-
-// func NewDevice(id int64) *Device {
-// 	return &Device{id: id}
-// }
-
-// func (d *Device) SetTargetType(target string) {
-// 	fmt.Println(d.id, "target type is", target)
-// }
-
-// func (d *Device) SetTargetValue(target float64) {
-// 	fmt.Println(d.id, "target value is", target)
-// }
-
-// func NewMotorController(config constantTypes.MotorController) *MotorController {
-// 	return &MotorController{
-// 		device:        Device{id: config.Id},
-// 		PidController: pid.NewPIDController(config.PID),
-// 	}
-// }
-
-// func (c *MotorController) SetTarget(val float64) {
-// 	c.Write(val)
-// 	c.PidController.SetTarget(val)
-// }
-
-// func (c *MotorController) GetValue() float64 {
-// 	return c.device.value
-// }
-
-// func (c *MotorController) GetTarget() float64 {
-// 	return c.PidController.GetTarget()
-// }
-
-// func (c *MotorController) GetId() int64 {
-// 	return int64(c.device.target)
-// }
-
-// func (c *MotorController) Run() {
-// 	c.PidController.Calculate(c.device.value)
-// }
-
-// func (c *MotorController) AtValue() bool {
-// 	// replace less than val later
-// 	return math.Abs(c.device.target-c.device.value) < 0.2
-// }
-
-// idk if this method works
-// func (c *MotorController) Write(val float64) {
-// 	// // Write 0x10 to the device, and read a byte right after.
-// 	// write := []byte{0x10, 0x00}
-// 	// buf := new(bytes.Buffer)
-// 	// err := binary.Write(buf, binary.LittleEndian, val)
-// 	// if err != nil {
-// 	// 	fmt.Println("could not convert float to bytes")
-// 	// 	return
-// 	// }
-// 	// write := append(buf.Bytes(), 0x00)
-// 	// read := make([]byte, len(write))
-// 	// if err := bus.spiPort.Tx(write, read); err != nil {
-// 	// 	log.Fatal(err)
-// 	// }
-// 	// fmt.Println(read[1:])
-// 	// WriteToCan(uint(c.device.id), []byte{})
-// }
 
 func SetBatteryConfig(conf constantTypes.Battery) {
 	config = conf
--- state/hardware/hardware_nonlinux.go
+++ state/hardware/hardware_nonlinux.go
@@ -4,94 +4,13 @@ package hardware
 
 import (
 	"github.com/LikeMyGames/FRC-Like-Robot/state/constantTypes"
-	"github.com/LikeMyGames/FRC-Like-Robot/state/pid"
 )
 
-type (
-	CanBus struct {
-		SPI_MISO_PIN int
-		SPI_MOSI_PIN int
-		SPI_SCLK_PIN int
-	}
-
-	CanNode struct {
-		id          int
-		registerMap map[string]int
-	}
-
-	Device struct {
-		id     int64
-		value  float64
-		target float64
-	}
-
-	MotorController struct {
-		device        Device
-		PidController *pid.PIDController
-	}
-)
-
-// var bus = NewCanBus(19, 21, 23)
+type ()
 
 var (
 	config constantTypes.Battery = constantTypes.Battery{}
 )
-
-func NewCanBus(MOSI, MISO, SCLK int) *CanBus {
-	return &CanBus{
-		SPI_MISO_PIN: MISO,
-		SPI_MOSI_PIN: MOSI,
-		SPI_SCLK_PIN: SCLK,
-	}
-}
-
-// func NewDevice(id int64) *Device {
-// 	return &Device{id: id}
-// }
-
-// func (d *Device) SetTargetType(target string) {
-// 	fmt.Println(d.id, "target type is", target)
-// }
-
-// func (d *Device) SetTargetValue(target float64) {
-// 	fmt.Println(d.id, "target value is", target)
-// }
-
-// func NewMotorController(config constantTypes.MotorController) *MotorController {
-// 	return &MotorController{
-// 		device:        Device{id: config.Id},
-// 		PidController: pid.NewPIDController(config.PID),
-// 	}
-// }
-
-// func (c *MotorController) SetTarget(val float64) {
-// 	c.PidController.SetTarget(val)
-// }
-
-// func (c *MotorController) GetValue() float64 {
-// 	return c.device.value
-// }
-
-// func (c *MotorController) GetTarget() float64 {
-// 	return c.device.target
-// }
-
-// func (c *MotorController) GetId() int64 {
-// 	return int64(c.device.target)
-// }
-
-// func (c *MotorController) Run() {
-// 	c.PidController.Calculate(c.device.value)
-// }
-
-// func (c *MotorController) AtValue() bool {
-// 	// replace less than val later
-// 	return math.Abs(c.device.target-c.device.value) < 0.2
-// }
-
-// func (c *MotorController) Write(val float64) {
-// 	fmt.Println("the write function for the MotorController struct only works in Linux, as such, it is only defined when the program is built in Linux")
-// }
 
 func SetConfig(cfg constantTypes.Battery) {
 	config = cfg
